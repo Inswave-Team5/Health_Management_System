70363edc3ff7cf536ef1d6f035fd67f9d91274ad
won0845 <tpqms775@gmail.com>
2025-03-06 17:07:05 +0900

    feat: add Coupon toString()

--- src/com/healthmanage/model/Coupon.java
+++ src/com/healthmanage/model/Coupon.java
@@ -4,9 +4,15 @@ public class Coupon {
 	private String number;
 	private boolean isUsed;
 	private int coinAmount;
-	Coupon(String number, int coinAmount){
+	public Coupon(String number, int coinAmount){
 		this.number = number;
 		this.isUsed = false;
 		this.coinAmount = coinAmount;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Coupon [쿠폰번호=" + number + ", 사용여부=" + isUsed + ", 보상코인=" + coinAmount + "]";
 	}
 }
