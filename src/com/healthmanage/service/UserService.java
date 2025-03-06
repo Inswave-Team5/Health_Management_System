9ac8d983213a9864e6db292777cf9c0a999c106b
won0845 <tpqms775@gmail.com>
2025-03-06 22:39:41 +0900

    feat: add Config file

--- src/com/healthmanage/service/UserService.java
+++ src/com/healthmanage/service/UserService.java
@@ -5,14 +5,14 @@ import com.healthmanage.model.Gym;
 import com.healthmanage.model.User;
 
 public class UserService {
-	
+
 	public boolean checkId(String userId) {
-		if(Gym.users.containsKey(userId)) {
+		if (Gym.users.containsKey(userId)) {
 			return false;
 		}
 		return true;
 	}
-	
+
 	public void addUser(String name, String password, String userId) {
 
 		Gym.users.put(userId, new User(name, password, userId));
@@ -22,16 +22,14 @@ public class UserService {
 		if (!Gym.users.containsKey(userId)) {
 			System.out.println("없는 아이디입니다.");
 			return false;
-		}else {
-			if(!Gym.users.get(userId).getPassword().equals(pw)) {
+		} else {
+			if (!Gym.users.get(userId).getPassword().equals(pw)) {
 				System.out.println("비밀번호가 일치하지 않습니다.");
 				return false;
-			}else {
+			} else {
 				System.out.println("로그인 성공");
 				return true;
 			}
 		}
-	
-	
-	
+	}
 }
