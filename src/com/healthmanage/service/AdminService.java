34a0f685bdd2f077934ee782608259a9a989e014
won0845 <tpqms775@gmail.com>
2025-03-06 17:07:50 +0900

    feat: add Coupon find, findAll, add

--- src/com/healthmanage/service/AdminService.java
+++ src/com/healthmanage/service/AdminService.java
@@ -1,5 +1,23 @@
 package com.healthmanage.service;
 
 public class AdminService {
+	private CouponService couponservice;
+	public AdminService() {
+		this.couponservice = CouponService.getInstance();
+	}
 	
+	
+	
+	
+	public void findAllCoupon() {
+		couponservice.findAllCoupons();
+	}
+	
+	public void addCoupon(String number, int coinAmount) {
+		couponservice.createCoupon(number, coinAmount);
+	}
+	public void findCoupon(String number) {
+		couponservice.findCoupon(number);
+		
+	}
 }
