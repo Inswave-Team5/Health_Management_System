7f26bfa65a562aabf53ff4c77afde9561ecff661
won0845 <tpqms775@gmail.com>
2025-03-06 23:05:35 +0900

    resolve conflict

--- src/com/healthmanage/app/Main.java
+++ src/com/healthmanage/app/Main.java
@@ -6,10 +6,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("초기 설정 중입니다.");
-<<<<<<< HEAD
-=======
 		System.out.println(EnvConfig.get("123"));
->>>>>>> 67c4db5 (EnvConfig)
+
 	}
 
 }
