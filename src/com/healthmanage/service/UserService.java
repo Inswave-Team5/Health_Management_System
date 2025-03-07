edcdbf084355b5fbbb9ce11f13cf587e6e9ec428
kyungwon2won2 <ykw1230@naver.com>
2025-03-07 15:51:40 +0900

    feat : modify weightService, AttendanceService

--- src/com/healthmanage/service/UserService.java
+++ src/com/healthmanage/service/UserService.java
@@ -1,6 +1,5 @@
 package com.healthmanage.service;
 
-import com.healthmanage.app.LoginUser;
 import com.healthmanage.dto.UserSignUpDTO;
 import com.healthmanage.model.Gym;
 import com.healthmanage.model.User;
@@ -32,8 +31,6 @@ public class UserService {
 
 	public boolean userLogin(String userId, String pw) {
 		if (Gym.users.containsKey(userId) && Gym.users.get(userId).getPassword().equals(pw)) {
-			//로그인 성공시 userid 전달
-			LoginUser.setLoginUserId(userId);
 			return true;
 		} else {
 			return false;
@@ -41,7 +38,7 @@ public class UserService {
 
 
 	}
-	
+
 	public String useCoupon(String couponNumber) {
 		return couponService.useCoupon(couponNumber);
 	}
