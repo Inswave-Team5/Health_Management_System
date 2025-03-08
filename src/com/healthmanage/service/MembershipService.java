af7a65f802c990fa86c4f03252e5590b44046e0c
luxsoo <luxsoo@naver.com>
2025-03-08 19:22:45 +0900

    membershiphash

--- src/com/healthmanage/service/MembershipService.java
+++ src/com/healthmanage/service/MembershipService.java
@@ -1,11 +1,16 @@
 package com.healthmanage.service;
 
 import java.util.HashMap;
+
 import java.util.Scanner;
 
 import com.healthmanage.model.User;
 
+import com.healthmanage.utils.SHA256;
+
 public class MembershipService {
+	
+	private AdminService adminservice = AdminService.getInstance();
 	
 	public void addMember() {
 		
@@ -16,6 +21,10 @@ public class MembershipService {
 		String id = "";
 		System.out.println("ID를 입력해주세요 :");
 		id = scan.nextLine();
+		if(!adminservice.isValidId(id)) {
+			System.out.println("ID 형식이 올바르지 않습니다. 영소문자+숫자 포함 5~12자를 입력해주세요.");
+		}
+		
 		if (user_dic.containsKey(id)) {
 			System.out.println("이미 존재하는 ID 입니다.");
 			return;
@@ -25,11 +34,18 @@ public class MembershipService {
 		System.out.println("패스워드를 입력해주세요 : ");
 		pw = scan.nextLine();
 		
+		if(!adminservice.isValidPw(pw)) {
+			System.out.println("패스워드 형식이 올바르지 않습니다. 대문자,숫자,소문자영문,특수문자 1개 이상 포함한 8~16자를 입력해주세요.");
+			return;
+		}
+		
+		String hashedPw = SHA256.encrypt(pw);
+		
 		String name ="";
 		System.out.println("이름을 입력해주세요 : ");
 		name = scan.nextLine();
 		
-		User user = new User(name, pw, id);
+		User user = new User(name, hashedPw, id);
 		user_dic.put(id, user);
 		System.out.println(name + "님 회원가입이 완료됐습니다.");
 
