57b98fcf2d2a87cca66c4f016f3ad538c0864766
won0845 <tpqms775@gmail.com>
2025-03-10 21:48:44 +0900

    feat : view -> adminView 변경 + start()함수 추가

--- src/com/healthmanage/controller/AdminController.java
+++ src/com/healthmanage/controller/AdminController.java
@@ -5,15 +5,34 @@ import java.util.Collection;
 import com.healthmanage.model.Coupon;
 import com.healthmanage.service.AdminService;
 import com.healthmanage.view.View;
+import com.healthmanage.view.AdminView;
 
 public class AdminController {
-	private View view;
+	private AdminView view;
 	private AdminService adminService;
 	AdminController(){
-		this.view = new View();
+		this.view = new AdminView();
 		this.adminService = AdminService.getInstance();
 	}
 	/*----------유저 정보 조회----*/
+	
+	public void start() {
+		int key = 0;
+		while ((key = Integer.parseInt(view.selectMenu())) != 0) {
+			switch (key) {
+			case 1:
+				/*
+				 * case 1: addBook(); break; case 2: removeBook(); break; case 3: searchBook();
+				 * break; case 4: listBook(); break; case 5: listISBN(); break; case 6: save();
+				 * break; case 7: load(); break;
+				 */
+			default:
+				System.out.println("잘못 선택하였습니다.");
+				break;
+			}
+		}
+		System.out.println("종료합니다...");
+	}
 	
 	
 	/*----------쿠폰 조회------*/
