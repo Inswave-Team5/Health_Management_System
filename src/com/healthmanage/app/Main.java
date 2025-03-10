a3ef8e82a95d3942d68fde338efbe427f5c68add
won0845 <tpqms775@gmail.com>
2025-03-10 22:13:34 +0900

    feat : Main.java 초기설정 문구 변경

--- src/com/healthmanage/app/Main.java
+++ src/com/healthmanage/app/Main.java
@@ -2,25 +2,21 @@ package com.healthmanage.app;
 import com.healthmanage.config.EnvConfig;
 import com.healthmanage.controller.GymController;
 import com.healthmanage.service.LogService;
-import com.healthmanage.controller.WeightController;
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("초기 설정 중입니다.");
-		System.out.println(EnvConfig.get("USER_FILE"));
-		System.out.println(EnvConfig.get("LOG_FILE_PATH"));
+		System.out.println(EnvConfig.get("APP_NAME") + " " +  EnvConfig.get("APP_VERSION") +" Starting...");
 		
 		LogService log = LogService.getInstance();
-		log.addLog("로그추가입니다.");
+		log.addLog(EnvConfig.get("APP_NAME") + " " +  EnvConfig.get("APP_VERSION") +" Starting...");
 		
 		log.logCleanUp();
 		
 		GymController gym = new GymController();
-//		gym.start();
+		gym.start();
 
-		WeightController weight = new WeightController();
-
-
+		//WeightController weight = new WeightController();
 	}
 
 }
