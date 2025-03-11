1bf3e34adf0644ee416bfa529de6f0a6479a9964
won0845 <tpqms775@gmail.com>
2025-03-11 12:31:45 +0900

    feat : add Logging in CoinService

--- src/com/healthmanage/service/CoinService.java
+++ src/com/healthmanage/service/CoinService.java
@@ -6,17 +6,16 @@ import com.healthmanage.dao.UserDAO;
 import com.healthmanage.model.Gym;
 
 public class CoinService {
-
 	private static CoinService instance;
+	private LogService logger;
 
 	private CoinService() {
-
+		this.logger = LogService.getInstance();
 	}
 
 	public static CoinService getInstance() {
 		if (instance == null) {
 			instance = new CoinService();
-
 		}
 		return instance;
 	}
@@ -25,25 +24,23 @@ public class CoinService {
 		int coin = Integer.parseInt(money);
 		if (coin > 0) {
 			user.setCoin(user.getCoin() + coin);
+			logger.addLog(user.getUserId()+"님에게"+money+"개 코인 충전되었습니다.");
 			return coin + " 코인 충전완료";
 		}
 		return "충전현금을 입력해주세요.";
-
 	}
 
 	// 이체(출금)한 유저, 이체받을(입금) 유저, 코인 파일에 저장
 	public String withdraw(String coin, User sender, User receiver) { // 유저 간 계좌 이체
-
 		int rcoin = Integer.parseInt(coin);
-
 		if (rcoin > 0 && sender.getCoin() >= rcoin) {
 			sender.setCoin(sender.getCoin() - rcoin);
 			receiver.setCoin(receiver.getCoin() + rcoin);
+			logger.addLog(sender.getUserId()+"님이 " + receiver.getUserId()+"님에게 "+coin+"원 이체하셨습니다.");
 			return "이체되었습니다.";
 		} else {
 			return "입력하신 코인을 다시 확인해주세요.";
 		}
-
 	}
 
 	// 코인 잔액 조회
