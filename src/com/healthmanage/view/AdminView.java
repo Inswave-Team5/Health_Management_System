3e3d1e37a79bd4b969c7d0d5200b31e7f1a7ebeb
leejihyeon114 <leejihyeon114>
2025-03-11 20:58:04 +0900

    refactor : menu 출력문 수정
    selectAdminMenu() 관리자모드 메뉴변경
    selectUserManageMenu() 회원관리 메뉴변경

--- src/com/healthmanage/view/AdminView.java
+++ src/com/healthmanage/view/AdminView.java
@@ -9,12 +9,17 @@ public class AdminView extends View {
 	
 	public String selectAdminMenu() {
 		return getInput("관리자 모드입니다. \n "
-				+ "1:회원관리 0:종료");
+				+ "1:회원관리 2:쿠폰관리 3:기구관리 0:종료");
 	}
 	
 	public String selectUserManageMenu() {
 		return getInput("회원관리. \n "
-				+ "1:전체조회 2:회원조회 3: 회원수정 4: 회원삭제 0:종료");
+				+ "1:전체조회 2:개인회원출결(날짜별) 3:개인회원출결(전체) 4:전체회원출결(날짜별) 5:전체회원운동누적시간 0:종료");
+	}
+	
+	public String selectCouponManageMenu() {
+		return getInput("쿠폰관리. \n "
+				+ "1:쿠폰발급 2:쿠폰조회 3:쿠폰삭제 0:종료");
 	}
 	
 	public void showRank(int rank, String userId, String workOutTime) {
