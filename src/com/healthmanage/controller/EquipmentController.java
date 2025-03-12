361fe9106878a98f71917b6fda6edc29d2d7a04e
won0845 <tpqms775@gmail.com>
2025-03-12 18:17:31 +0900

    feat : UserMenu운동 기구 모드에서 빈 문자열 입력시 예외처리

--- src/com/healthmanage/controller/EquipmentController.java
+++ src/com/healthmanage/controller/EquipmentController.java
@@ -8,33 +8,40 @@ import com.healthmanage.service.EquipmentService;
 import com.healthmanage.view.UserView;
 
 public class EquipmentController {
-    private final EquipmentService equipmentService;
-    private final UserView view;
+	private final EquipmentService equipmentService;
+	private final UserView view;
 
-    public EquipmentController() {
-        equipmentService = EquipmentService.getInstance();
-        view = new UserView();
-    }
+	public EquipmentController() {
+		equipmentService = EquipmentService.getInstance();
+		view = new UserView();
+	}
 
-    public void equipmentEntry() {
-        int key = 0;
-        while (Gym.isLoggedIn() && (key = Integer.parseInt(view.EquipmentSelectMenu())) != 0) {
-            view.showMessage(key + "번 입력되었습니다.");
-            switch (key) {
-                case 1:
-                    useEquipment();
-                    break;
-                case 2:
-                    viewWorkoutHistory();
-                    break;
-                case 0:
-                    break;
-                default:
-                    view.showMessage("잘못 선택하였습니다.");
-                    break;
-            }
-        }
-    }
+	public void equipmentEntry() {
+		int key = 0;
+		while (Gym.isLoggedIn()) {
+			try {
+				key = Integer.parseInt(view.EquipmentSelectMenu());
+			} catch (NumberFormatException e) {
+				view.showAlert("숫자로된 메뉴 번호를 입력해주세요");
+				continue;
+			}
+			view.showMessage(key + "번 입력되었습니다.");
+			switch (key) {
+			case 1:
+				useEquipment();
+				break;
+			case 2:
+				viewWorkoutHistory();
+				break;
+			case 0:
+				view.showAlert("종료합니다.");
+				return;
+			default:
+				view.showMessage("잘못 선택하였습니다.");
+				break;
+			}
+		}
+	}
 
 	public void useEquipment() {
 		if (Gym.isLoggedIn()) {
@@ -42,14 +49,14 @@ public class EquipmentController {
 			return;
 		}
 
-        String equipmentType = view.getInput("사용할 운동 기구 입력: ");
-        int value1 = Integer.parseInt(view.getInput("속도 or 무게 입력: "));
-        int value2 = Integer.parseInt(view.getInput("시간 or 횟수 입력: "));
+		String equipmentType = view.getInput("사용할 운동 기구 입력: ");
+		int value1 = Integer.parseInt(view.getInput("속도 or 무게 입력: "));
+		int value2 = Integer.parseInt(view.getInput("시간 or 횟수 입력: "));
 
 		equipmentService.addWorkoutRecord(equipmentType, value1, value2);
 
-        view.showMessage("운동기구 사용이 정상 기록되었습니다!");
-    }
+		view.showMessage("운동기구 사용이 정상 기록되었습니다!");
+	}
 
 	public void viewWorkoutHistory() {
 		if (!Gym.isLoggedIn()) {
