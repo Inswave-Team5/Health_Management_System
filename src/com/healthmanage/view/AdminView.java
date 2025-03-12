3c55d8c48e52ef28e535fefd686f2d0ff95f9e55
won0845 <tpqms775@gmail.com>
2025-03-12 14:26:31 +0900

    feat : view 수정

--- src/com/healthmanage/view/AdminView.java
+++ src/com/healthmanage/view/AdminView.java
@@ -1,28 +1,49 @@
 package com.healthmanage.view;
 
 public class AdminView extends View {
-	public AdminView() {
-		super();
-	}
+    public AdminView() {
+        super();
+    }
 
-	public String selectEntryMenu() {
-		return getInput("1. 로그인 2. 관리자등록 0. 종료");
-	}
+    public String selectEntryMenu() {
+        return getMenuInput("ADMIN LOGIN", 
+                        "[1] 로그인\n" +
+                        "[2] 관리자 등록\n" +
+                        "[0] 종료");
+    }
 
-	public String selectAdminMenu() {
-		return getInput("관리자 모드입니다. \n " + "1:회원관리 2:쿠폰관리 3:기구관리 0:종료");
-	}
+    public String selectAdminMenu() {
+        return getMenuInput("ADMIN MODE",
+                        "[1] 회원 관리\n" +
+                        "[2] 쿠폰 관리\n" +
+                        "[3] 기구 관리\n" +
+                        "[0] 종료");
+    }
 
-	public String selectUserManageMenu() {
-		return getInput("회원관리. \n " + "1:전체조회 2:개인회원출결(날짜별) 3:개인회원출결(전체) 4:전체회원출결(날짜별) 5:전체회원운동누적시간 0:종료");
-	}
+    public String selectUserManageMenu() {
+        return getMenuInput("MEMBER MANAGEMENT", 
+                        "[1] 전체 조회\n" +
+                        "[2] 개인회원 출결 (날짜별)\n" +
+                        "[3] 개인회원 출결 (전체)\n" +
+                        "[4] 전체 회원 출결 (날짜별)\n" +
+                        "[5] 전체 회원 운동 누적 시간\n" +
+                        "[0] 종료");
+    }
 
-	public String selectCouponManageMenu() {
-		return getInput("쿠폰관리. \n " + "1:쿠폰발급 2:쿠폰조회 3:쿠폰삭제 0:종료");
-	}
+    public String selectCouponManageMenu() {
+        return getMenuInput("COUPON MANAGEMENT",
+                        "[1] 쿠폰 발급\n" +
+                        "[2] 쿠폰 조회\n" +
+                        "[3] 쿠폰 삭제\n" +
+                        "[0] 종료");
+    }
 
-	public void showRank(int rank, String userId, String workOutTime) {
-		System.out.println("랭킹 " + rank + "등 아이디 : " + userId + " 누적 시간 : " + workOutTime);
-	}
-
+    public void showRank(int rank, String userId, String workOutTime) {
+        System.out.println("\n===================================");
+        System.out.printf("          🏆 랭킹 %d위%n", rank);
+        System.out.println("===================================");
+        System.out.printf("👤 아이디 : %s%n", userId);
+        System.out.printf("⏳ 누적 운동 시간 : %s%n", workOutTime);
+        System.out.println("===================================\n");
+    }
 }
--- src/com/healthmanage/view/UserView.java
+++ src/com/healthmanage/view/UserView.java
@@ -1,40 +1,72 @@
 package com.healthmanage.view;
 
 public class UserView extends View {
-	public UserView() {
-		super();
-	}
+    public UserView() {
+        super();
+    }
 
-	public String selectMenu() {
-		return getInput("사용자 모드입니다. \n " + "1:나의 출결 2:운동시간 3:체중관리 4:운동기구 5:쿠폰등록 6:코인 7:비밀번호 변경 0:로그아웃");
-	}
+    public String selectMenu() {
+        return getMenuInput("USER MODE", 
+                        "[1] 나의 출결\n" +
+                        "[2] 운동 시간\n" +
+                        "[3] 체중 관리\n" +
+                        "[4] 운동 기구\n" +
+                        "[5] 쿠폰 등록\n" +
+                        "[6] 코인 관리\n" +
+                        "[7] 비밀번호 변경\n" +
+                        "[0] 로그아웃");
+    }
 
-	public String selectLogin() {
-		return getInput("1. 로그인 2. 회원가입 0. 종료");
-	}
+    public String selectLogin() {
+        return getMenuInput("LOGIN / SIGN UP",
+                        "[1] 로그인\n" +
+                        "[2] 회원가입\n" +
+                        "[0] 종료");
+    }
 
-	public String AttendanceSelectMenu() {
-		return getInput("[나의 출결] 메뉴입니다. \n " + "1:입장 등록  2:퇴장 등록  3:나의 입퇴장  0:메인메뉴");
-	}
+    public String AttendanceSelectMenu() {
+        return getMenuInput("CHECK-IN MENU",
+                        "[1] 입장 등록\n" +
+                        "[2] 퇴장 등록\n" +
+                        "[3] 나의 입퇴장 기록\n" +
+                        "[0] 메인 메뉴");
+    }
 
-	public String WeightSelectMenu() {
-		return getInput("[나의 몸무게] 메뉴입니다. \n " + "1:몸무게 등록  2:몸무게 조회  0:메인메뉴");
-	}
+    public String WeightSelectMenu() {
+        return getMenuInput("WEIGHT MANAGEMENT",
+                        "[1] 몸무게 등록\n" +
+                        "[2] 몸무게 조회\n" +
+                        "[0] 메인 메뉴");
+    }
 
-	public String WeightSelectCheckMenu() {
-		return getInput("[몸무게 조회] 메뉴입니다. \n " + "1:전체 조회  2:월별 조회  3:날짜 조회  0:뒤로가기");
-	}
+    public String WeightSelectCheckMenu() {
+        return getMenuInput("WEIGHT CHECK",
+                        "[1] 전체 조회\n" +
+                        "[2] 월별 조회\n" +
+                        "[3] 날짜 조회\n" +
+                        "[0] 뒤로 가기");
+    }
 
-	public String TimeSelectMenu() {
-		return getInput("[운동 시간] 메뉴입니다. \n " + "1:오늘의 운동시간  2:날짜별 조회  3:월별 누적 조회  4.전체 누적 조회  0:메인메뉴");
-	}
+    public String TimeSelectMenu() {
+        return getMenuInput("WORKOUT TIME",
+                        "[1] 오늘의 운동 시간\n" +
+                        "[2] 날짜별 조회\n" +
+                        "[3] 월별 누적 조회\n" +
+                        "[4] 전체 누적 조회\n" +
+                        "[0] 메인 메뉴");
+    }
 
-	public String EquipmentSelectMenu() {
-		return getInput("[운동 기구] 메뉴입니다. \n " + "1:기구 사용 등록  2:기구 사용 조회  0:메인메뉴");
-	}
+    public String EquipmentSelectMenu() {
+        return getMenuInput("GYM EQUIPMENT",
+                        "[1] 기구 사용 등록\n" +
+                        "[2] 기구 사용 조회\n" +
+                        "[0] 메인 메뉴");
+    }
 
-	public String coinSelectMenu() {
-		return getInput("[코인] 메뉴입니다. \n " + "1:코인 충전  2:코인 선물  0:메인메뉴");
-	}
-
+    public String coinSelectMenu() {
+        return getMenuInput("COIN MENU",
+                        "[1] 코인 충전\n" +
+                        "[2] 코인 선물\n" +
+                        "[0] 메인 메뉴");
+    }
 }
