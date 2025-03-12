344b0c513d412708e052b148a68a0db52f760000
won0845 <tpqms775@gmail.com>
2025-03-12 16:24:52 +0900

    Merge Conflict

--- src/com/healthmanage/service/AdminService.java
+++ src/com/healthmanage/service/AdminService.java
@@ -54,38 +54,6 @@ public class AdminService {
 		logger.addLog(EnvConfig.get("ADMIN_FILE") + " File SAVE");
 	}
 
-	public String memberSearch(String memberNum) { // 회원 검색조회
-		if (Gym.users.containsKey(memberNum)) {
-			return Gym.users.get(memberNum).toString();
-		} else {
-			return null;
-		}
-	}
-
-	public boolean pwChange(String memberNum, String pw) { // 비밀번호 수정
-
-		User user = Gym.users.get(memberNum);
-		if (user == null) {
-			adminView.showMessage("사용자를 찾을 수 없습니다.");
-			return false;
-		}
-
-		if (!SHA256.verifyPassword(pw, user.getSalt(), user.getPassword())) {
-			adminView.showMessage("비밀번호가 올바르지 않습니다.");
-			return false;
-		}
-
-		String newPw = adminView.getInput("새로운 비밀번호를 입력하세요.");
-		String newSalt = SHA256.generateSalt();
-		String newHashedPw = SHA256.hashPassword(newPw, newSalt);
-
-		user.setPassword(newHashedPw, newSalt);
-
-		adminView.showMessage("비밀번호가 성공적으로 변경되었습니다.");
-		logger.addLog(memberNum + "님의 비밀번호가 변경되었습니다.");
-		return true;
-	}
-
 //	public void memberDelete(String memberNum) { // 삭제
 //		Gym.users.remove(memberNum);
 //		logger.addLog(memberNum + "님의 User정보가 삭제되었습니다.");
