0b9434b02abd6f20f665f5d9a995520f44abfbba
won0845 <tpqms775@gmail.com>
2025-03-14 14:41:59 +0900

    feat : weight 필드 private + getter,setter 생성
    몸무게 전체 조회시 날짜별 정렬

--- src/com/healthmanage/model/Weight.java
+++ src/com/healthmanage/model/Weight.java
@@ -4,11 +4,19 @@ import java.io.Serializable;
 
 public class Weight implements Serializable {
 
-    public String userId;   // 로그인한 userId
-    public String date;     // 날짜 - 입력당시 날짜 자동 기입
-    public String weight;    // 몸무게
+	private String userId;   // 로그인한 userId
+    private String date;     // 날짜 - 입력당시 날짜 자동 기입
+    private String weight;    // 몸무게
 
-    public Weight(String userId, String date, String weight) {
+    public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public Weight(String userId, String date, String weight) {
         this.userId = userId;
         this.date = date;
         this.weight = weight;
